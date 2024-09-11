b483c0156a5569689f2459d2457a55068ba85b21
Tim <mindthehug@googlemail.com>
2024-09-11 09:49:52 +0000

    adapted the script to parametrise data input and to ingest a second df to postgres

--- Homework/Module_1_Homework/ingest_data.py
+++ Homework/Module_1_Homework/ingest_data.py
@@ -16,20 +16,26 @@ def main(params):
     host = params.host 
     port = params.port 
     db = params.db
-    table_name = params.table_name
-    url = params.url
+    table_name_1 = params.table_name_1
+    table_name_2 = params.table_name_2
+    url_1 = params.url_1
+    url_2 = params.url_2
     
-    # The data will be read from the directory. According to best practice, this file is not pushed to the git repo
-    # To reproduce this script, one would therefore have to donwload the file using the link below, then unzip it and
-    # store the file in the same directory as this script. 
+    # ===================================================================================================
+    # Info on the urls (which are the download links for the data)
+    # url_1:
     # https://github.com/DataTalksClub/nyc-tlc-data/releases/download/green/green_tripdata_2019-09.csv.gz
-
-    csv_name = "green_tripdata_2019-09.csv"
+    # --> Contains data about taxi trips
     
+    # url_2:
+    # https://github.com/DataTalksClub/nyc-tlc-data/releases/download/misc/taxi_zone_lookup.csv
+    # --> Contains data about taxi zones
+    # ===================================================================================================
+
     engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{db}')
 
     # Reading the data in chunks of 100000
-    df_iter = pd.read_csv(csv_name, iterator=True, chunksize=100000)
+    df_iter = pd.read_csv(url_1, iterator=True, chunksize=100000, compression= "gzip")
 
     df = next(df_iter)
 
@@ -38,9 +44,9 @@ def main(params):
     df.lpep_dropoff_datetime = pd.to_datetime(df.lpep_dropoff_datetime)
 
     # Ingesting the data into postgres 
-    df.head(n=0).to_sql(name=table_name, con=engine, if_exists='replace')
+    df.head(n=0).to_sql(name=table_name_1, con=engine, if_exists='replace')
 
-    df.to_sql(name=table_name, con=engine, if_exists='append')
+    df.to_sql(name=table_name_1, con=engine, if_exists='append')
 
 
     while True: 
@@ -53,17 +59,23 @@ def main(params):
             df.lpep_pickup_datetime = pd.to_datetime(df.lpep_pickup_datetime)
             df.lpep_dropoff_datetime = pd.to_datetime(df.lpep_dropoff_datetime)
 
-            df.to_sql(name=table_name, con=engine, if_exists='append')
+            df.to_sql(name=table_name_1, con=engine, if_exists='append')
 
             t_end = time()
 
             print('inserted another chunk, took %.3f second' % (t_end - t_start))
 
         except StopIteration:
-            print("Finished ingesting data into the postgres database")
+            print("Finished ingesting taxi trip data into the postgres database")
             break
 
 
+    # Ingesting taxi zone data 
+    df_taxi_zones = pd.read_csv(url_2)
+
+    df_taxi_zones.to_sql(name = table_name_2, con = engine, if_exists='replace')
+
+    print("Finished ingesting taxi zone data into the postgres database")
 
 # We want dockerise this script (run it as a docker container). For this we also created a Dockerfile
 # which we will use to build the docker image. This docker file is in the same directory as this script.
@@ -77,8 +89,11 @@ if __name__ == '__main__':
     parser.add_argument('--host', required=True, help='host for postgres')
     parser.add_argument('--port', required=True, help='port for postgres')
     parser.add_argument('--db', required=True, help='database name for postgres')
-    parser.add_argument('--table_name', required=True, help='name of the table where we will write the results to')
-    parser.add_argument('--url', required=True, help='url of the csv file')
+    parser.add_argument('--table_name_1', required=True, help='name of the first table where we will write the results to')
+    parser.add_argument('--table_name_2', required=True, help='name of the second table where we will write the results to')
+    parser.add_argument('--url_1', required=True, help='url of the first csv file')
+    parser.add_argument('--url_2', required=True, help='url of the second csv file')
+
 
     args = parser.parse_args()
 
